1b51e973d99950ca5a2a5931d3481ed15252057a
Jinhan Choi <jinhanchoi1@gmail.com>
2017-05-12 00:52:26 +0900

    start pjt

---
+++ VisualStudioTestPjt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudioTestPjt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
